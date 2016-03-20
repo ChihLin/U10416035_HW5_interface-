b199ce386d7bdf8ff10ee9224306051c7e5cc2b3
ChihLin <azs128821@gmail.com>
2016-03-21 01:40:38 +0800

    add the explanation
    give all the code an explanation

--- BeanGame.java
+++ BeanGame.java
@@ -10,6 +10,7 @@ public class BeanGame extends Application{
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {
 	
+		//add the line of the shape
 		Polyline line = new Polyline();
 		line.getPoints().addAll(155d,40d,155d,60d,70d,195d,70d,230d);
 		
@@ -19,6 +20,8 @@ public class BeanGame extends Application{
 		Polyline line3 = new Polyline();
 		line3.getPoints().addAll(70d,230d,260d,230d);
 		
+		
+		// add the ball of the shape
 		Circle c = new Circle(5);
 		c.relocate(160d,70d);
 		Circle c1 = new Circle(5);
@@ -77,6 +80,8 @@ public class BeanGame extends Application{
 		Circle c27 = new Circle(5);
 		c27.relocate(232d,190d);
 		
+		
+		//add the bottom line of the bean game
 		Polyline line4 = new Polyline();
 		line4.getPoints().addAll(93d,190d,93d,230d);
 		Polyline line5 = new Polyline();
@@ -92,13 +97,16 @@ public class BeanGame extends Application{
 		Polyline line10 = new Polyline();
 		line10.getPoints().addAll(237d,190d,237d,230d);
 		
+		//to new a group (children)
 		Group g = new Group();
 		
+		//to add the all line into the shape
 		g.getChildren().add(line);
 		g.getChildren().add(line2);
 		g.getChildren().add(line3);
 		g.getChildren().addAll(line4,line5,line6,line7,line8,line9,line10);
 		
+		//add the circle into the shape
 		g.getChildren().addAll(c,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27);
 	
 		//Create a scene and place it in the stage
